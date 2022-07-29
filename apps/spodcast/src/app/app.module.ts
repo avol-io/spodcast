a9866ebc84342ddb8ff3534f4b3b5723a733c2c4
Avol.io <social@avol.io>
2022-07-29 19:00:31 +0200

    feat(layout): added the empty layout component

--- apps/spodcast/src/app/app.module.ts
+++ apps/spodcast/src/app/app.module.ts
@@ -4,23 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AuthModule, AuthResolver } from '@spodcast/auth';
+import { SharedModule } from '@spodcast/shared';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './componets/layout/layout.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, LayoutComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     AuthModule,
+    SharedModule,
     RouterModule.forRoot([
       {
         path: '',
+        component: LayoutComponent,
         resolve: { auth: AuthResolver },
         children: [
           {
             path: 'user',
             loadChildren: () => import('@spodcast/user').then((m) => m.UserModule),
+          },
+          {
+            path: 'shows',
+            loadChildren: () => import('@spodcast/shows').then((m) => m.ShowsModule),
           },
         ],
       },
---
+++ apps/spodcast/src/app/componets/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class LayoutComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
