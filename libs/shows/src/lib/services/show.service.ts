1138ac3f8d5471d563ce9805b7aa98fbb9bb22ef
Avol.io <social@avol.io>
2022-08-02 21:28:32 +0200

    refactor(shows): move calls for playlist in its own module

--- libs/shows/src/lib/services/show.service.ts
+++ libs/shows/src/lib/services/show.service.ts
@@ -2,8 +2,7 @@
 
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { SPOTIFY_CONF } from '@spodcast/shared';
-import { concatMap, filter, map, of, range, takeWhile } from 'rxjs';
+import { SPOTIFY_CONF, UserService } from '@spodcast/shared';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +10,7 @@ import { concatMap, filter, map, of, range, takeWhile } from 'rxjs';
 export class ShowService {
   playlistExist: string | undefined = undefined;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private userService: UserService) {}
 
   getShows(page = 1) {
     const size = 50;
@@ -29,40 +28,5 @@ export class ShowService {
   getNextEpisodes(next: string) {
     return this.http.get<SpotifyApi.PagingObject<SpotifyApi.EpisodeObjectSimplified>>(next);
     //https://api.spotify.com/v1/shows/1FaCiqGahURjjO42JOMiyd
-  }
-
-  createSpodcastPlaylist() {}
-
-  findSpodcastPlaylist() {
-    if (this.playlistExist) {
-      return of(true);
-    }
-    const url = SPOTIFY_CONF.API.PLAYLIST;
-    return range(0, 1000000).pipe(
-      concatMap((page) => {
-        const size = 50;
-        const offset = (page - 1) * size;
-        const queryParam = new HttpParams().set('offset', offset).set('limit', size);
-        return this.http.get<SpotifyApi.ListOfCurrentUsersPlaylistsResponse>(url, { params: queryParam });
-      }),
-      takeWhile(
-        (response) =>
-          !!response.next &&
-          !!response.items.find((p) => {
-            if (p.name == SPOTIFY_CONF.PLAYLIST_SPODCAST) {
-              this.playlistExist = p.id;
-              return false;
-            } else {
-              return true;
-            }
-          })
-      ),
-      filter((response) => {
-        return !!this.playlistExist || !response.next; //fai passare appena trovi la playlist o se non c'è più next così vuoto
-      }),
-      map(() => {
-        return this.playlistExist;
-      })
-    );
   }
 }
